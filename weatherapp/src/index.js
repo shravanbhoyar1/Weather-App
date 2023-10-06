0a0270101d66924457884514500b1cba6ca19b8f
shravanbhoyar1 <shravanbhoyar3507>
2023-10-06 18:41:21 +0530

    added weather app

--- weatherapp/src/index.js
+++ weatherapp/src/index.js
@@ -1,8 +1,9 @@
-
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Home from './view/Home/Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <home/>
+ <Home/>
 );
-
--- weatherapp/src/view/Home/Home.js
+++ weatherapp/src/view/Home/Home.js
@@ -1,25 +1,49 @@
-import React, { useState } from "react";
-import "./Home.css"
-import axios from 'axios';
-function Home() {
-    const [city, setCity] = useState('pune')
-    const [temperature, setTemperature] = useState(0)
-    function loadWeatherInfo(){
-        const response = axios.get('https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=f652964084c552e8c0492237a3fabd9c')
+import React,{ useEffect, useState } from "react";
+import axios from "axios";
+import './Home.css';
+
+function Home(){
+    const [city,setCity] = useState('nagpur');
+    const [temprature,setTemprature] = useState(0);
+    const [message,setMessage] = useState('')
+
+    async function loadweatherInfo(){
+        try{
+        const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=f652964084c552e8c0492237a3fabd9c');
+
+        setTemprature((response.data.main.temp -273).toFixed(2));
+        setMessage('✅city Found');
     }
+    catch(err){
+        setTemprature(0);
+        setMessage('city not found');
+    }
+}
+
+    useEffect(()=>{
+        loadweatherInfo()
+    },[city])
+
     return(
         <div>
+            <h1 className="app-title">whether for {city}</h1>
 
-            <h1 className="app-title">Weather For {city}</h1>
-            <input className="search-bar" 
-             type="text"
-             placeholder="Enter city..."
-             value={city}
-             onChange={(e) =>{
+            <input 
+            className="search-bar" 
+            type="text" 
+            placeholder="Enter your city..."
+
+            value={city}
+            onChange={(e) =>{
                 setCity(e.target.value)
-             }}/>
-
-            <h1 className="temperature-text">Temperature: {temperature} °C</h1>
+            }}
+            
+            />
+            <p className="message-text">{message}</p>
+            <h1 className="temprature-text">Temprature: {temprature} °C</h1>
+            
         </div>
     )
 }
+
+export default Home
